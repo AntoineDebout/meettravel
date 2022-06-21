bbef967c4081f0a09fc75d55483db2da77665dab
Antoine DEBOUT <antoine.debout.95@gmail.com>
2022-06-21 02:11:02 +0200

    Added Favicon

--- resources/js/modules/home/pages/index.vue
+++ resources/js/modules/home/pages/index.vue
@@ -3,7 +3,7 @@
         class="text-3xl font-bold text-center"
     >
         <font-awesome-icon icon="fa-brands fa-adn" />
-         Welcome on Meet Traveler
+         Welcome on Meet & Travel
         <font-awesome-icon icon="fa fa-user-secret" />
     </h1>
 </template>
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,8 +3,10 @@
 <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
+    <link rel="icon" href="{{ URL::asset('favicon.png') }}" type="image/x-icon"/>
     <link href="{{ mix('/css/app.css') }}" rel="stylesheet" />
     <script src="{{ mix('/js/app.js') }}" defer></script>
+    <title>Meet&Travel</title>
     @inertiaHead
 </head>
 <body>
